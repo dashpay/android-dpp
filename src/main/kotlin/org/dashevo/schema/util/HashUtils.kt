be67b9004111892cd3aee07e635f0e1fbbeae94e
Samuel Barbosa <sambarbosaa@gmail.com>
2018-07-18 19:45:49 -0500

    Using CBOR to generate Object Hash

--- src/main/kotlin/org/dashevo/schema/util/HashUtils.kt
+++ src/main/kotlin/org/dashevo/schema/util/HashUtils.kt
@@ -1,30 +1,13 @@
 package org.dashevo.schema.util
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.cbor.CBORFactory
+import com.fasterxml.jackson.datatype.jsonorg.JsonOrgModule
 import org.bitcoinj.core.Sha256Hash
 import org.json.JSONObject
 import java.io.ByteArrayOutputStream
-import java.nio.charset.Charset
 
 object HashUtils {
-
-    private fun buildTree(obj: JSONObject, tree: ArrayList<ByteArray> = arrayListOf()): ArrayList<ByteArray> {
-        obj.keys().forEach { key ->
-            if (obj[key] is JSONObject) {
-                buildTree(obj.getJSONObject(key), tree)
-                return@forEach
-            }
-            val ba = ("{" + "\"key\":" + (
-                    if (obj[key] is String) {
-                        ("\"" + obj[key] + "\"")
-                    } else {
-                        obj[key]
-                    })+"}").toByteArray(Charset.forName("UTF-8"))
-            val hash = ba.asList().reversed().toByteArray()
-            tree.add(hash)
-        }
-
-        return tree
-    }
 
     /**
      * Return a double SHA256 hash of a Schema object instance
@@ -32,34 +15,14 @@ object HashUtils {
      * @returns {*|string}
      */
     fun toHash(obj: JSONObject): String {
-        val tree = buildTree(obj)
-        var j = 0
-        var size = tree.size
-        while (size > 1) {
-            var i = 0
-            while (i < size) {
-                val i2 = Math.min(i + 1, size - 1)
-                val buf = ByteArrayOutputStream()
-                buf.write(tree[j + i])
-                buf.write(tree[j + i2])
-                tree.add(Sha256Hash.hashTwice(buf.toByteArray()))
-                i += 2
-            }
-            j += size
-            size = Math.floor(((size + 1) / 2).toDouble()).toInt()
-        }
-
-        //TODO: * What to do when data is smaller than 32 bytes
-        if (tree.last().size < 32) {
-            val byteArray = ByteArray(32)
-            var i = 0
-            tree.last().iterator().forEach {
-                byteArray[i++] = it
-            }
-            tree[tree.lastIndex] = byteArray
-        }
-
-        return Sha256Hash.wrapReversed(tree.last()).toString()
+        val cborFactory = CBORFactory()
+        val mapper =  ObjectMapper(cborFactory)
+        mapper.registerModule(JsonOrgModule())
+        val byteArr = mapper.writeValueAsBytes(obj.toMap())
+        //TODO: Jackson's Cbor seems to be adding additional bytes to the end
+        //byteArr = Arrays.copyOfRange(byteArr, 0, byteArr.size-2)
+        return Sha256Hash.wrap(Sha256Hash.hashTwice(byteArr)).toString()
+        //Old code in: https://gist.github.com/sambarboza/db5c4d6880f057e899bccd70c846d3d6
     }
 
     fun toHash(objList: List<JSONObject>): String {
