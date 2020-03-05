75ffec2222fd2370e85866836e970c23293f0f67
HashEngineering <hashengineeringsolutions@gmail.com>
2020-03-04 21:15:57 -0800

    Add BaseObject to handle serialization and hash functions

---
+++ src/main/kotlin/org/dashevo/dpp/BaseObject.kt
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2020-present, Dash Core Group Team
+ *
+ * This source code is licensed under the MIT license found in the
+ * COPYING file in the root directory of this source tree.
+ */
+package org.dashevo.dpp
+
+import org.dashevo.dpp.util.HashUtils
+
+/**
+ * The abstract base class for Dash Platform objects that will handle
+ * serialization and hashing of the object data
+ */
+
+abstract class BaseObject {
+
+    /**
+     * Return plain JSON object
+     *
+     */
+    abstract fun toJSON(): Map<String, Any>
+
+    /**
+     * Return serialized object
+     *
+     */
+    fun serialize(): ByteArray {
+        return HashUtils.encode(this.toJSON())
+    }
+
+    /**
+     * Return the double SHA256 hash of the serialized object
+     *
+     */
+    fun hash(): String {
+        return HashUtils.toHash(this.serialize()).toHexString()
+    }
+}
