5f89cfcd9b3f62bd4b26f027169bc3ec36cf9b71
HashEngineering <hashengineeringsolutions@gmail.com>
2020-03-17 16:24:45 -0700

    IdentityCreateTransition: Add identityId

--- src/main/kotlin/org/dashevo/dpp/identity/IdentityCreateTransition.kt
+++ src/main/kotlin/org/dashevo/dpp/identity/IdentityCreateTransition.kt
@@ -6,22 +6,33 @@
  */
 package org.dashevo.dpp.identity
 
+import org.dashevo.dpp.toBase64
+import org.dashevo.dpp.util.HashUtils
+
 class IdentityCreateTransition(var identityType: Identity.IdentityType?,
                                var lockedOutPoint: String?,
                                var publicKeys: List<IdentityPublicKey>,
                                protocolVersion: Int = 0)
     : IdentityStateTransition(Types.IDENTITY_CREATE, protocolVersion) {
 
+    var identityId: String
+
+    init {
+        identityId = HashUtils.toHash(HashUtils.fromBase64(lockedOutPoint!!)).toBase64()
+    }
+
     constructor(rawStateTransition: MutableMap<String, Any>)
             : this(rawStateTransition["identityType"] as? Identity.IdentityType,
             rawStateTransition["lockedOutPoint"] as String,
-            (rawStateTransition["publicKeys"] as List<Any>).map { entry -> IdentityPublicKey(entry as MutableMap<String, Any>) }, 0)
+            (rawStateTransition["publicKeys"] as List<Any>).map { entry -> IdentityPublicKey(entry as MutableMap<String, Any>) }, 0) {
+        identityId = HashUtils.toHash(HashUtils.fromBase64(lockedOutPoint!!)).toBase64()
+    }
 
     override fun toJSON(skipSignature: Boolean): Map<String, Any> {
         var json = super.toJSON() as MutableMap<String, Any>
         json["identityType"] = identityType as String
         json["lockedOutPoint"] = lockedOutPoint as String
-        json["publicKeys"] = publicKeys.map {}
+        json["publicKeys"] = publicKeys.map { it.toJSON() }
         return json
     }
 }
