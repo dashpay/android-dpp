a9a7cbc0171b2d955c7b9cba7711a6a26f90921c
HashEngineering <hashengineeringsolutions@gmail.com>
2020-03-17 17:09:20 -0700

    ContractStateTransition: Prevent Recursion in toJSON

--- src/main/kotlin/org/dashevo/dpp/contract/ContractStateTransition.kt
+++ src/main/kotlin/org/dashevo/dpp/contract/ContractStateTransition.kt
@@ -5,7 +5,7 @@ import org.dashevo.dpp.statetransition.StateTransition
 class ContractStateTransition(var contract: Contract) : StateTransition(Types.DATA_CONTRACT) {
 
     override fun toJSON(skipSignature: Boolean): Map<String, Any> {
-        var json = super.toJSON() as MutableMap<String, Any>
+        var json = super.toJSON(skipSignature) as MutableMap<String, Any>
         json["dataContract"] = contract
         return json
     }
