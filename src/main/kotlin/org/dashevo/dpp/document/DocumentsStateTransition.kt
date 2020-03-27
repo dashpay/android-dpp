88a19f2eeee3d46628132ebf95ca4df616ec6542
HashEngineering <hashengineeringsolutions@gmail.com>
2020-03-26 23:01:43 -0700

    DocumentsStateTransition: Use correct type and prevent recursion

--- src/main/kotlin/org/dashevo/dpp/document/DocumentsStateTransition.kt
+++ src/main/kotlin/org/dashevo/dpp/document/DocumentsStateTransition.kt
@@ -2,10 +2,10 @@ package org.dashevo.dpp.document
 
 import org.dashevo.dpp.statetransition.StateTransition
 
-class DocumentsStateTransition(var documents: List<Document>): StateTransition(Types.DATA_CONTRACT) {
+class DocumentsStateTransition(var documents: List<Document>): StateTransition(Types.DOCUMENTS) {
 
     override fun toJSON(skipSignature: Boolean): Map<String, Any> {
-        var json = super.toJSON() as MutableMap<String, Any>
+        var json = super.toJSON(skipSignature) as MutableMap<String, Any>
         json["actions"] = documents.map { entry -> entry.action }
         json["documents"] = documents.map { entry -> entry.toJSON() }
         return json
