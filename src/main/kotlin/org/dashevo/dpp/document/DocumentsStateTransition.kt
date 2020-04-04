40485ee0ba4238e99c8b5ec40ab2f9a634986832
HashEngineering <hashengineeringsolutions@gmail.com>
2020-04-04 08:34:19 -0700

    DocumentsStateTransition: Save the value of the action enum to the json map

--- src/main/kotlin/org/dashevo/dpp/document/DocumentsStateTransition.kt
+++ src/main/kotlin/org/dashevo/dpp/document/DocumentsStateTransition.kt
@@ -16,7 +16,7 @@ class DocumentsStateTransition : StateTransition {
 
     override fun toJSON(skipSignature: Boolean): Map<String, Any> {
         var json = super.toJSON(skipSignature) as MutableMap<String, Any>
-        json["actions"] = documents.map { entry -> entry.action }
+        json["actions"] = documents.map { entry -> entry.action.value }
         json["documents"] = documents.map { entry -> entry.toJSON() }
         return json
     }
