cf3353267afc3a0ce1184908e21003b2821e724a
HashEngineering <hashengineeringsolutions@gmail.com>
2020-04-04 08:57:23 -0700

    Add DataProvider interface

---
+++ src/main/kotlin/org/dashevo/dpp/DataProvider.kt
@@ -0,0 +1,36 @@
+package org.dashevo.dpp
+
+import org.dashevo.dpp.contract.Contract
+import org.dashevo.dpp.document.Document
+import org.dashevo.dpp.identity.Identity
+
+
+/**
+ *
+ */
+interface DataProvider {
+    /**
+     * Fetch Data Contract by ID
+     *
+     */
+    fun fetchDataContract(contractId: String): Contract?
+
+    /**
+     * Fetch Documents by Data Contract ID and type
+     *
+     */
+    fun fetchDocuments(contractId: String, type: String, where: Any): List<Document>
+
+    /**
+     * Fetch transaction by ID
+     *
+     */
+    fun fetchTransaction(id: String): Int
+
+    /**
+     * Fetch identity by ID
+     *
+     */
+    fun fetchIdentity(id: String): Identity?
+}
+
