df175f9687c788475971e22c5f079d537f1e461a
HashEngineering <hashengineeringsolutions@gmail.com>
2020-09-19 08:45:02 -0700

    Remove obsolete tests and test data files

--- src/test/kotlin/org/dashevo/dpp/BaseTest.kt
+++
@@ -1,9 +0,0 @@
-package org.dashevo.dpp
-
-import org.json.JSONObject
-import java.io.File
-
-open class BaseTest {
-    val data: JSONObject = JSONObject(File("src/test/resources/data/contactsdap-test-data.json").readText())
-    val contactsDap: JSONObject = JSONObject(File("src/test/resources/data/contactsdap.json").readText())
-}
