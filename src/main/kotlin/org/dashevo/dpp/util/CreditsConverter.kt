657aa42b847d141a638fb934d2e7c81cfb2ec3ac
HashEngineering <hashengineeringsolutions@gmail.com>
2021-01-05 09:37:25 -0500

    Add CreditsConverter object

---
+++ src/main/kotlin/org/dashevo/dpp/util/CreditsConverter.kt
@@ -0,0 +1,20 @@
+package org.dashevo.dpp.util
+
+import org.bitcoinj.core.Coin
+import kotlin.math.floor
+
+object CreditsConverter {
+    const val RATIO = 1000
+
+    fun convertSatoshiToCredits(amount: Long): Long {
+        return amount * RATIO
+    }
+
+    fun convertSatoshiToCredits(amount: Coin): Long {
+        return amount.value * RATIO
+    }
+
+    fun convertCreditsToSatoshis(amount: Long): Long {
+        return floor(amount.toDouble() / RATIO).toLong()
+    }
+}
