da50f9e368fe09e1f489aff5a54e89e2723797da
HashEngineering <hashengineeringsolutions@gmail.com>
2021-09-10 20:11:58 -0700

    feat: add ProtocolVersion object

---
+++ src/main/kotlin/org/dashj/platform/dpp/ProtocolVersion.kt
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2021-present, Dash Core Team
+ *
+ * This source code is licensed under the MIT license found in the
+ * COPYING file in the root directory of this source tree.
+ */
+
+package org.dashj.platform.dpp
+
+object ProtocolVersion {
+    const val latestVersion = 1
+
+    val compatibility = mapOf(1 to 1)
+}
