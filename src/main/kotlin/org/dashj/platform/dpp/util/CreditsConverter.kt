dc10b977ebb0b8bac2049ee3048ba0bc75b3cc7d
HashEngineering <hashengineeringsolutions@gmail.com>
2021-09-15 19:52:47 -0700

    refactor: make CreditsConverter.RATIO private

--- src/main/kotlin/org/dashj/platform/dpp/util/CreditsConverter.kt
+++ src/main/kotlin/org/dashj/platform/dpp/util/CreditsConverter.kt
@@ -1,10 +1,17 @@
+/**
+ * Copyright (c) 2020-present, Dash Core Team
+ *
+ * This source code is licensed under the MIT license found in the
+ * COPYING file in the root directory of this source tree.
+ */
+
 package org.dashj.platform.dpp.util
 
 import org.bitcoinj.core.Coin
 import kotlin.math.floor
 
 object CreditsConverter {
-    const val RATIO = 1000
+    private const val RATIO = 1000
 
     fun convertSatoshiToCredits(amount: Long): Long {
         return amount * RATIO
