6c9fb4d2316b333940ebb093b89c3fa605716618
HashEngineering <hashengineeringsolutions@gmail.com>
2021-12-24 15:17:27 -0800

    docs: add license to DashPlatformProtocol

--- src/main/kotlin/org/dashj/platform/dpp/DashPlatformProtocol.kt
+++ src/main/kotlin/org/dashj/platform/dpp/DashPlatformProtocol.kt
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2020-present, Dash Core Team
+ *
+ * This source code is licensed under the MIT license found in the
+ * COPYING file in the root directory of this source tree.
+ */
+
 package org.dashj.platform.dpp
 
 import org.bitcoinj.core.Context
