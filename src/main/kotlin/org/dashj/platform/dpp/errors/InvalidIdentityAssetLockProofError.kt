3fa15f720beb258197b6b4a784fc195ac129a9db
HashEngineering <hashengineeringsolutions@gmail.com>
2021-12-27 21:09:28 -0800

    feat: add InvalidInstantAssetLockProofException
    * this replaces InvalidIdentityAssetLockProofError

--- src/main/kotlin/org/dashj/platform/dpp/errors/InvalidIdentityAssetLockProofError.kt
+++
@@ -1,4 +0,0 @@
-package org.dashj.platform.dpp.errors
-
-class InvalidIdentityAssetLockProofError(message: String) :
-    Exception("Invalid asset lock proof: $message")
---
+++ src/main/kotlin/org/dashj/platform/dpp/errors/concensus/basic/identity/InvalidInstantAssetLockProofException.kt
@@ -0,0 +1,9 @@
+package org.dashj.platform.dpp.errors.concensus.basic.identity
+
+import org.dashj.platform.dpp.errors.concensus.ConcensusException
+
+class InvalidInstantAssetLockProofException(message: String) : ConcensusException("Invalid instant lock proof: $message") {
+    constructor(arguments: List<Any>) : this(arguments[0] as String) {
+        setArguments(arguments)
+    }
+}
