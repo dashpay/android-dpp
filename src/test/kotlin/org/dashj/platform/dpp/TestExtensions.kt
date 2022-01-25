831dd4cf23f1536660df9bc8bbf11a7417db1314
HashEngineering <hashengineeringsolutions@gmail.com>
2022-01-25 14:03:01 -0800

    test: add assertListEquals which uses List.deepCompare

--- src/test/kotlin/org/dashj/platform/dpp/TestExtensions.kt
+++ src/test/kotlin/org/dashj/platform/dpp/TestExtensions.kt
@@ -22,3 +22,10 @@ fun assertMapNotEquals(expected: Map<String, Any?>, actual: Map<String, Any?>) {
         throw AssertionFailedError(null, expected, actual)
     }
 }
+
+fun assertListEquals(expected: List<Any>, actual: List<Any>) {
+    val result = expected.deepCompare(actual)
+    if (!result) {
+        throw AssertionFailedError(null, expected, actual)
+    }
+}
