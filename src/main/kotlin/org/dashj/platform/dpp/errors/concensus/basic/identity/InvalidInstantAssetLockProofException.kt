a914dfc2b207f0c0dcdc21244564ea6ed661f436
HashEngineering <hashengineeringsolutions@gmail.com>
2022-01-25 14:12:18 -0800

    style: reformat InvalidInstantAssetLockProofException

--- src/main/kotlin/org/dashj/platform/dpp/errors/concensus/basic/identity/InvalidInstantAssetLockProofException.kt
+++ src/main/kotlin/org/dashj/platform/dpp/errors/concensus/basic/identity/InvalidInstantAssetLockProofException.kt
@@ -2,7 +2,8 @@ package org.dashj.platform.dpp.errors.concensus.basic.identity
 
 import org.dashj.platform.dpp.errors.concensus.ConcensusException
 
-class InvalidInstantAssetLockProofException(message: String) : ConcensusException("Invalid instant lock proof: $message") {
+class InvalidInstantAssetLockProofException(message: String) :
+    ConcensusException("Invalid instant lock proof: $message") {
     constructor(arguments: List<Any>) : this(arguments[0] as String) {
         setArguments(arguments)
     }
